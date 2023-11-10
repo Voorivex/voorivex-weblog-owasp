cf99c9bab00ca33e95d507ef4f34f5048b44955b
Voorivex <y.shahinzadeh@gmail.com>
2023-11-10 13:13:17 +0000

    ssrf + directory traversal fix

--- get_image.php
+++ get_image.php
@@ -1,5 +1,9 @@
 <?php
 $imagePath = $_GET['imgsrc'];
-$imageData = file_get_contents($imagePath);
+# $imagePath = str_replace('../', '', $imagePath);
+if (strstr($imagePath, '../') != False) {
+    die('security failed!');
+}
+$imageData = file_get_contents('/var/www/html/' . $imagePath);
 echo $imageData;
 ?>
