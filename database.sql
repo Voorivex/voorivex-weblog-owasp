4ec99caa51e626f2437f9c62440e03117d4655da
YShahinzadeh <y.shahinzadeh@gmail.com>
2025-02-06 16:58:26 +0300

    .

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.41, for Linux (x86_64)
 --
--- Host: localhost    Database: voorivex_weblog
+-- Host: localhost    Database: webapp
 -- ------------------------------------------------------
--- Server version	8.0.34-0ubuntu0.22.04.1
+-- Server version	8.0.41-0ubuntu0.24.10.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,9 +25,9 @@ DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `category_id` int NOT NULL AUTO_INCREMENT,
   `category_name` varchar(255) NOT NULL,
-  `ategory_description` varchar(2000) NOT NULL,
+  `category_description` varchar(2000) NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,6 +36,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'hacking','posts about hacking'),(2,'general','general posts');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,7 +77,7 @@ CREATE TABLE `posts` (
   `category_id` int DEFAULT NULL,
   `publication_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -85,6 +86,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'Hacker','Yallah Haj Yashar',1,1,'2025-02-06 12:35:07'),(2,'Yallah ??','Yallah Haj Yashar',1,1,'2025-02-06 12:35:30'),(3,'Hacker','Pourya#2025-Feb-6',1,1,'2025-02-06 12:36:28'),(4,'Pourya#2025-Feb-6','',1,1,'2025-02-06 12:36:44'),(5,'<Img Src=OnXSS OnError=confirm(\"xss\")>','<Img Src=OnXSS OnError=confirm(\"xss\")>',1464,1,'2025-02-06 13:57:05');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -132,7 +134,7 @@ CREATE TABLE `users` (
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `token` (`token`)
-) ENGINE=InnoDB AUTO_INCREMENT=1455 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1468 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,7 +143,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'voorivex','y.shahinzadeh@gmail.com','Yashar','Shahinzadeh','I\'m a hacker','123qwe!@#QWE','920dba9f387a7505649c66560eb3833d','2023-09-16 14:19:59'),(2,'mamad','mamad@gmail.com',NULL,NULL,NULL,'mamad_secure','null','2023-09-16 15:15:48'),(3,'test1','test@test.com',NULL,NULL,NULL,'test',NULL,'2023-09-24 10:07:53'),(11,'test2','test2@gmail.com',NULL,NULL,NULL,'test2',NULL,'2023-09-24 10:37:26'),(12,'bTKt','',NULL,NULL,NULL,'Gsdk',NULL,'2023-09-25 18:59:06'),(1449,'moein','moeinefn@gmail.com',NULL,NULL,NULL,'nosafe','b268d93183a51579632d540bfe2ff5d7','2023-09-27 12:57:26'),(1452,'a','b',NULL,NULL,NULL,'0',NULL,'2023-10-01 16:19:05'),(1453,'mamad12321','mamad12321@gmail.com',NULL,NULL,NULL,'mamad12321',NULL,'2023-10-19 13:31:33'),(1454,'sos','sos@sos.com','\">','','\">','sos',NULL,'2023-10-27 10:24:55');
+INSERT INTO `users` VALUES (1,'voorivex','y.shahinzadeh@gmail.com','Yashar','Shahinzadeh','Yallah, Haj yashar hoodi haye ma chid shod 😔 #owasp-zero-b','123qwe!@#QWE','920dba9f387a7505649c66560eb3833d','2023-09-16 14:19:59'),(2,'mamad','mamad@gmail.com',NULL,NULL,NULL,'mamad_secure','null','2023-09-16 15:15:48'),(3,'test1','test@test.com',NULL,NULL,NULL,'test',NULL,'2023-09-24 10:07:53'),(11,'test2','test2@gmail.com',NULL,NULL,NULL,'test2',NULL,'2023-09-24 10:37:26'),(12,'bTKt','',NULL,NULL,NULL,'Gsdk',NULL,'2023-09-25 18:59:06'),(1449,'moein','moeinefn@gmail.com',NULL,NULL,NULL,'nosafe','b268d93183a51579632d540bfe2ff5d7','2023-09-27 12:57:26'),(1452,'a','b',NULL,NULL,NULL,'0',NULL,'2023-10-01 16:19:05'),(1453,'mamad12321','mamad12321@gmail.com',NULL,NULL,NULL,'mamad12321',NULL,'2023-10-19 13:31:33'),(1454,'sos','sos@sos.com','\">','','\">','sos',NULL,'2023-10-27 10:24:55'),(1455,'sajjad.null','asdfsadf@gmail.com',NULL,NULL,NULL,'123qwe',NULL,'2025-02-06 11:36:29'),(1456,'VAHID','gerdwiesler1@gmail.com',NULL,NULL,NULL,'123456',NULL,'2025-02-06 11:36:41'),(1457,'hacker','hacker@gmail.com',NULL,NULL,NULL,'123456',NULL,'2025-02-06 11:42:14'),(1458,'\" order by 1000//','asda@gmail.com',NULL,NULL,NULL,'safda',NULL,'2025-02-06 12:31:35'),(1459,'uname','moz@gmail.com',NULL,NULL,NULL,'wename',NULL,'2025-02-06 12:35:33'),(1461,'yasharisthebest','ys@gmail.com',NULL,NULL,NULL,'yowasp11403',NULL,'2025-02-06 12:42:30'),(1462,'mamadhacker','mamadhacker@gmail.com',NULL,NULL,NULL,'123qwe!@#QWE',NULL,'2025-02-06 12:43:04'),(1463,'hoody','1231@gmail.com',NULL,NULL,NULL,'asdfasfda',NULL,'2025-02-06 12:46:35'),(1464,'hamoon0a','crypto3@gmail.com',NULL,NULL,NULL,'fuckinghell',NULL,'2025-02-06 13:03:35'),(1465,'sajjad_nul','sajjad@hoodi.com',NULL,NULL,NULL,'هوددددددییییی',NULL,'2025-02-06 13:04:28'),(1466,'test','example@gmail.com',NULL,NULL,NULL,'12345',NULL,'2025-02-06 13:41:39'),(1467,'I want a hoodie.','sajad@gmail.com',NULL,NULL,NULL,'sajad',NULL,'2025-02-06 13:44:23');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -154,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-29 15:21:09
+-- Dump completed on 2025-02-06 13:57:48
